5f1a9d47e39a30cef9051691e4411292d1df2284
Sadegh Mehdizadeh <sdg.mhz@gmail.com>
2024-08-05 21:51:20 +0330

    add BookCreateView

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.urls import reverse_lazy
 
 from .models import Book
 
@@ -13,4 +14,12 @@ class BookDetailView(generic.DetailView):
 	model = Book
 	# context_object_name = 'book'
 	template_name = 'books/book_detail.html'
+
+
+class BookCreateView(generic.CreateView):
+	model = Book
+	fields = '__all__'
+	template_name = 'books/book_create.html'
+	# success_url = reverse_lazy('book_list')
+
 
