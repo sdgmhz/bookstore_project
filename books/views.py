fab632aecdfa42c66c20fb39176515566d9975ed
Sadegh Mehdizadeh <sdg.mhz@gmail.com>
2024-08-07 11:39:00 +0330

    add BookUpdateView and BookDeleteView

--- books/views.py
+++ books/views.py
@@ -23,3 +23,18 @@ class BookCreateView(generic.CreateView):
 	# success_url = reverse_lazy('book_list')
 
 
+class BookUpdateView(generic.UpdateView):
+	model = Book
+	fields = ['title', 'author', 'description']
+	template_name = 'books/book_update.html'
+
+
+class BookDeleteView(generic.DeleteView):
+	model = Book
+	template_name = 'books/book_delete.html'
+	success_url = reverse_lazy('book_list')
+
+
+
+
+
