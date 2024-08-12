44cdf0193d965aabc6b34a81710e3a9d42ec32bf
Sadegh Mehdizadeh <sdg.mhz@gmail.com>
2024-08-12 16:48:26 +0330

    add frontend template and static files

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -144,4 +144,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 # Email config
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# static files config
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+
+
 
